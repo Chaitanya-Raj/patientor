34b81e99f08828e81120e212ad65e0ef7c00ae2e
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2022-05-05 11:07:20 +0530

    9.17

---
+++ src/PatientPage/index.tsx
@@ -0,0 +1,55 @@
+import axios from "axios";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { apiBaseUrl } from "../constants";
+import { useStateValue } from "../state";
+import { Gender, Patient } from "../types";
+import MaleIcon from "@mui/icons-material/Male";
+import FemaleIcon from "@mui/icons-material/Female";
+import TransgenderIcon from "@mui/icons-material/Transgender";
+
+const PatientPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const [{ patients }, dispatch] = useStateValue();
+  React.useEffect(() => {
+    const fetchPatient = async () => {
+      try {
+        const { data: patient } = await axios.get<Patient>(
+          // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch({ type: "SET_PATIENT", payload: patient });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    void fetchPatient();
+  }, [dispatch]);
+
+  return (
+    <div className="Patient">
+      {id && (
+        <>
+          <h1>
+            {patients[id].name}{" "}
+            {patients[id].gender == Gender.Male ? (
+              <MaleIcon />
+            ) : patients[id].gender == Gender.Female ? (
+              <FemaleIcon />
+            ) : (
+              <TransgenderIcon />
+            )}
+          </h1>
+
+          <p>
+            ssn: {patients[id].ssn}
+            <br />
+            occupation: {patients[id].occupation}
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default PatientPage;
